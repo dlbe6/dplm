0d5e2298609279a3920c428729d412cd16910afd
Kuftyrev Vyacheslav <you@example.com>
2018-05-06 20:59:46 +0300

    finished index

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,32 @@
+$("nav").on("click", "a", function () {
+   $(this).addClass("current").siblings().removeClass("current")
+   $(".photo img").attr("src", $(this).prop("href"))
+   return false;
+})
+var fixOwl = function(){
+        var $stage = $('.owl-stage'),
+            stageW = $stage.width(),
+            $el = $('.owl-item'),
+            elW = 0;
+        $el.each(function() {
+            elW += $(this).width()+ +($(this).css("margin-right").slice(0, -2))
+        });
+        if ( elW > stageW ) {
+            $stage.width( elW );
+        };
+    }
 $(function() {
 
 	// Custom JS
 	$(".owl-carousel").owlCarousel({
-    items:1,
-    loop: true,
-    nav:true
+        // margin:10,
+        // autoWidth:true,
+        items:1,
+        loop:true,
+        nav: true,
+        navText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>'],
+        onInitialized: fixOwl,
+        onRefreshed: fixOwl
     
 	});
 
